2010d672054c809578e64f24d5633584cfa89b91
Lazokat <137065848+Lazokat@users.noreply.github.com>
2023-09-03 17:26:42 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.template.defaulttags import url
-from django.conf.urls.static import static
+from django.conf.urls.static import static,serve
 from drf_spectacular.views import (
 SpectacularAPIView,
 SpectacularRedocView,
