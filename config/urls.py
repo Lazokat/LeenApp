fa1f5f8015f0121d97909059a8b8c76fe3626ea3
Lazokat <137065848+Lazokat@users.noreply.github.com>
2023-09-03 17:52:54 +0500

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static,serve
+from django.conf.urls.static import static
 from drf_spectacular.views import (
 SpectacularAPIView,
 SpectacularRedocView,
@@ -41,5 +41,3 @@ include("dj_rest_auth.registration.urls")),
     path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(
         url_name="schema"), name="swagger-ui"),
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-if not settings.DEBUG:
-    urlpatterns += [ path(r'^uploads/(?P<path>.)$', serve,{'document_root': settings.MEDIA_ROOT}), path(r'^static/(?P<path>.)$', serve,{'document_root': settings.STATIC_ROOT}), ]
